90a0d8c051003ae1a03b8e031670d6b10e7ecbc1
kwigbo <jlaudenslayer@gmail.com>
2023-05-23 17:42:02 -0400

    Fixed random load when no metamask

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 
 # Default values of arguments
 DEPLOY_PROD=0
+DEPLOY_STAGE=0
+DEPLOY_ALL=0
 
 # Loop through arguments and process them
 for arg in "$@"
@@ -9,6 +11,18 @@ do
     case $arg in
         -p|--production)
         DEPLOY_PROD=1
+        shift
+        ;;
+    esac
+    case $arg in
+        -s|--stage)
+        DEPLOY_STAGE=1
+        shift
+        ;;
+    esac
+    case $arg in
+        -a|--all)
+        DEPLOY_ALL=1
         shift
         ;;
     esac
@@ -29,12 +43,15 @@ cp -r favicon build
 
 cd build
 
-if [ $DEPLOY_PROD -eq 1 ]
+if [ $DEPLOY_STAGE -eq 1 ] || [ $DEPLOY_ALL -eq 1 ]
+then
+   echo "Push to Stage"
+   aws s3 sync . s3://kwigbelle-stage --delete
+fi
+
+if [ $DEPLOY_PROD -eq 1 ] || [ $DEPLOY_ALL -eq 1 ]
 then
    echo "Push to Production"
    aws s3 sync s3://kwigbelle-stage s3://kwigbelle --delete
-   aws cloudfront create-invalidation --distribution-id ETU79Z47QN0GQ --paths "/*"
-else
-   echo "Push to Stage"
-   aws s3 sync . s3://kwigbelle-stage --delete
+   aws cloudfront create-invalidation --distribution-id EMDM091I7VR9X --paths "/*"
 fi
